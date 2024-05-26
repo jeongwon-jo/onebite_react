e8f32b44eb92114ff8a5c88102d47386b76886ad
jeongwon-jo <129711334+jeongwon-jo@users.noreply.github.com>
2024-05-26 18:26:55 +0900

    [update] section03 마무리

---
+++ section03/src/index.js
@@ -0,0 +1,11 @@
+// const { add, sub } = require("./math")
+import mul, {add, sub} from "./math.js"
+
+import randomColor from "randomcolor";
+// console.log(add(1,2));
+// console.log(sub(1,2));
+// console.log(mul(2, 3));
+
+const color = randomColor()
+console.log(color);
+
